18a24a760f5ad0f01458f357c8a71af7f53b9fe5
Elhadj Alimou <diallomamadoualimou@gmail.com>
2025-07-09 11:57:07 +0200

    Initial commit - Spring Boot e-commerce backend

---
+++ src/main/java/org/ecommerce/functionnalityecommerce/FunctionnalityEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.ecommerce.functionnalityecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionnalityEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionnalityEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ecommerce/functionnalityecommerce/FunctionnalityEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.functionnalityecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionnalityEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
